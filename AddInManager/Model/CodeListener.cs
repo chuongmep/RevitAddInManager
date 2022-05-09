aa76c97601dd68693c7504175c227f06d2863d43
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-05-09 11:26:17 +0700

    add code listener

---
+++ AddInManager/Model/CodeListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace RevitAddinManager.Model;
+
+public class CodeListener  : TraceListener
+{
+    public override void Write(string message)
+    {
+        using (StreamWriter st = new StreamWriter(DefaultSetting.PathLogFile, true))
+        {
+            st.Write(message);
+            st.Close();
+        }
+    }
+
+    public override void WriteLine(string message)
+    {
+        using (StreamWriter st = new StreamWriter(DefaultSetting.PathLogFile, true))
+        {
+            st.Write(message);
+            st.Close();
+        }
+    }
+}
--- AddInManager/Model/DefaultSetting.cs
+++ AddInManager/Model/DefaultSetting.cs
@@ -1,4 +1,7 @@
-﻿namespace RevitAddinManager.Model;
+﻿using System.IO;
+using System.Net;
+
+namespace RevitAddinManager.Model;
 
 /// <summary>
 /// All setting name default for addin
@@ -10,10 +13,24 @@ public static class DefaultSetting
     public static string FormatExAddin = ".addin";
     public static string FormatDisable = ".disable";
     public static string AdskPath = "Autodesk\\Revit\\Addins";
-    public static string PathLogFile = @"C:\Users\Chuong.Ho\Documents\test.txt";
     public static string IniName = "revit.ini";
-
     public static string TempFolderName = "RevitAddins";
+    public static string DirLogFile = Path.Combine(Path.GetTempPath(), TempFolderName);
 
+    private static string pathLogFile;
+    public static string PathLogFile
+    {
+        get
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(DirLogFile);
+            FileInfo fileInfo = directoryInfo.GetFiles("*.txt",SearchOption.TopDirectoryOnly).OrderBy(x=>x.LastWriteTime).LastOrDefault();
+            if (fileInfo==null)
+            {
+                pathLogFile = Path.Combine(DirLogFile, $"{Guid.NewGuid()}.txt");
+                File.Create(pathLogFile).Close();
+            }
+            return pathLogFile;
+        }
+    }
     public static string AimInternalName = "AimInternal.ini";
 }
