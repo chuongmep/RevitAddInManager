51b05b7d3ef00389db2546b5a98d370b41460ab1
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-06-02 10:56:50 +0700

    improve key down up and close form by keyboard

--- AddInManager/View/FrmAddInManager.xaml.cs
+++ AddInManager/View/FrmAddInManager.xaml.cs
@@ -60,6 +60,40 @@ public partial class FrmAddInManager : Window
 
     private void HandleTreeViewCommandKeyPress(object sender, KeyEventArgs e)
     {
+        int indexCmd = TreeViewCommand.Items.IndexOf(TreeViewCommand.SelectedItem);
+        if (e.Key == Key.Up && TabCommand.IsFocused)
+        {
+            tbxSearch.Focus();
+        }
+        else if (e.Key == Key.Up && indexCmd==0 && TabCommand.IsSelected)
+        {
+            TabCommand.Focus();
+        }
+        if (e.Key == Key.Down && TabCommand.IsSelected)
+        {
+            TreeViewCommand.Focus();
+        }
+        if (e.Key == Key.Enter)
+        {
+            viewModel.ExecuteAddinCommandClick();
+        }
+
+    }
+    private void HandleTreeViewAppKeyPress(object sender, KeyEventArgs e)
+    {
+        int indexCmd = TreeViewApp.Items.IndexOf(TreeViewApp.SelectedItem);
+        if (e.Key == Key.Up && TabApp.IsFocused)
+        {
+            tbxSearch.Focus();
+        }
+        else if (e.Key == Key.Up && indexCmd==0 && TabApp.IsSelected)
+        {
+            TabApp.Focus();
+        }
+        if (e.Key == Key.Down && TabApp.IsSelected)
+        {
+            TreeViewApp.Focus();
+        }
         if (e.Key == Key.Enter)
         {
             viewModel.ExecuteAddinCommandClick();
@@ -89,5 +123,10 @@ public partial class FrmAddInManager : Window
             }
         }
         
+    }
+
+    private void CloseFormEvent(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape) Close();
     }
 }
