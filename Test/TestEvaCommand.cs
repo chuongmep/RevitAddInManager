4ea81aa5ffcbc2539a80ace8b71b2539b8365855
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-06-30 09:48:25 +0700

    Create TestEvaCommand.cs

---
+++ Test/TestEvaCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Test;
+
+[Transaction(TransactionMode.Manual)]
+public class TestEvaCommand  : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        
+        UIApplication uiApplication = commandData.Application;
+        MessageBox.Show(uiApplication.Application.Username);
+        return Result.Succeeded;
+    }
+}
