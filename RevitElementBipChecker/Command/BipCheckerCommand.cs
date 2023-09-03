b8579427023ee3a5412360914576a84ab4aecf5f
chuongmep <31106432+chuongmep@users.noreply.github.com>
2023-09-03 20:30:02 +0800

    refactor

--- RevitElementBipChecker/Command/BipCheckerCommand.cs
+++ RevitElementBipChecker/Command/BipCheckerCommand.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
-using KellermanSoftware.CompareNetObjects;
-using ObjectsComparer;
 using RevitElementBipChecker.Model;
 using RevitElementBipChecker.View;
 using RevitElementBipChecker.Viewmodel;
-using Difference = KellermanSoftware.CompareNetObjects.Difference;
 
 namespace RevitElementBipChecker.Command
 {
@@ -26,32 +18,11 @@ namespace RevitElementBipChecker.Command
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
-            
-      
-            
-
-            // write to csv
-            
-            // DataTable dataTable = new DataTable();
-            // dataTable.Columns.Add("Name");
-            // dataTable.Columns.Add("Type");
-            // dataTable.Columns.Add("Value1");
-            // dataTable.Columns.Add("Value2");
-            // foreach (var difference in differences)
-            // {
-            //     DataRow dataRow = dataTable.NewRow();
-            //     dataRow["Name"] = difference.Name;
-            //     dataRow["Type"] = difference.Type;
-            //     dataRow["Value1"] = difference.Value1;
-            //     dataRow["Value2"] = difference.Value2;
-            //     dataTable.Rows.Add(dataRow);
-            // }
-            // dataTable.ExportCsv();
-            // BipCheckerViewmodel vm = new BipCheckerViewmodel(uidoc);
-            // FrmBipChecker frMainWindows = new FrmBipChecker(vm);
-            // frMainWindows.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            // frMainWindows.SetRevitAsWindowOwner();
-            // frMainWindows.Show();
+            BipCheckerViewmodel vm = new BipCheckerViewmodel(uidoc);
+            FrmBipChecker frMainWindows = new FrmBipChecker(vm);
+            frMainWindows.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            frMainWindows.SetRevitAsWindowOwner();
+            frMainWindows.Show();
             return Result.Succeeded;
         }
         
