5e7b7df90132f0a471bfc0590bf9574a4a552b04
chuongmep <31106432+chuongmep@users.noreply.github.com>
2023-09-05 19:56:55 +0800

    allow sort by color

--- RevitElementBipChecker/View/FrmCompareBip.xaml.cs
+++ RevitElementBipChecker/View/FrmCompareBip.xaml.cs
@@ -1,4 +1,11 @@
-﻿using RevitElementBipChecker.Viewmodel;
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using RevitElementBipChecker.Viewmodel;
 
 namespace RevitElementBipChecker.View
 {
@@ -7,11 +14,31 @@ namespace RevitElementBipChecker.View
     /// </summary>
     public partial class FrmCompareBip
     {
+        private ParameterCompareViewModel _viewModel { get; set; }
+
         public FrmCompareBip(ParameterCompareViewModel viewModel)
         {
             InitializeComponent();
+            _viewModel = viewModel;
             this.DataContext = viewModel;
             viewModel.FrmCompareBip = this;
+        }
+
+        private void SortByColor(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var button = (Button) sender;
+                // sort another remain order by Name 
+                dataGrid.ItemsSource = _viewModel.Differences
+                    .OrderByDescending(x => x?.RowColor == button?.Background)
+                    .ThenBy(x => x.Name);
+                dataGrid.ScrollIntoView(dataGrid.Items[0]);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
         }
     }
 }
--- RevitElementBipChecker/View/FrmPropertyChecker.xaml.cs
+++ RevitElementBipChecker/View/FrmPropertyChecker.xaml.cs
@@ -1,5 +1,8 @@
-﻿using RevitElementBipChecker.Viewmodel;
+﻿using System;
+using System.Linq;
+using RevitElementBipChecker.Viewmodel;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace RevitElementBipChecker.View
 {
@@ -20,6 +23,21 @@ namespace RevitElementBipChecker.View
         private void CloseClick(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+        private void SortByColor(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var button = (Button) sender;
+                dataGrid.ItemsSource = _viewModel.Differences
+                    .OrderByDescending(x => x?.RowColor == button?.Background)
+                    .ThenBy(x => x.Name);
+                dataGrid.ScrollIntoView(dataGrid.Items[0]);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
         }
     }
 }
